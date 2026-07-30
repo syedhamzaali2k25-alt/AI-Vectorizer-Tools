a2a17cc5428bef56aee94f8b2a20a27e90960dae
syedhamzaali2k25-alt <syedhamzaali2k25@gmail.com>
2026-07-30 22:20:39 +0500

    wowowow

--- server/server.js
+++ server/server.js
@@ -107,21 +107,12 @@ const cleanUrlMap = {
   '/signup': 'signup.html',
   '/oauth-complete': 'oauth-complete.html'
 };
+app.get("/", (req, res) => {
+    const filePath = path.join(__dirname, "..", "client", "pages", "index.html");
 
-for (const [cleanPath, realFile] of Object.entries(cleanUrlMap)) {
-  app.get(cleanPath, (req, res) => {
-    const filePath = path.join(__dirname, '..', 'client', 'pages', realFile);
-
-    console.log("Trying to serve:", filePath);
-
-    res.sendFile(filePath, (err) => {
-      if (err) {
-        console.error(err); // <-- Ye line add karo
-        console.error(`[clean-url diagnostic] Failed to serve "${req.path}" -> expected file: ${filePath}`);
-      }
-    });
-  });
-}
+    res.type("html");
+    res.send(fs.readFileSync(filePath, "utf8"));
+});
 
 app.use(express.static(path.join(__dirname, '..', 'client')));
 
